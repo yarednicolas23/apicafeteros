c3d8d1fc471082f1b3f12c6a0581026eb73693ed
yarednicolas23 <yarednicolas@gmail.com>
2023-11-09 11:25:23 -0500

    add: express session

--- index.js
+++ index.js
@@ -1,33 +1,39 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const fs = require('fs');
-const cors = require("cors");
+const session = require('express-session');
 
 const app = express();
 const PORT = 3000;
 
-app.use(bodyParser.json()); // support json encoded bodies
-app.use(
-  bodyParser.urlencoded({
-    extended: true,
-  })
-);
+// Configuración de express-session
+app.use(session({
+  secret: 'my-secret-key',
+  resave: false,
+  saveUninitialized: true
+}));
 
-app.use(cors());
-app.use(express.json());
+app.use(bodyParser.json());
+
+// Middleware para verificar si hay una lista de marcas en la sesión, si no, la inicializa
+app.use((req, res, next) => {
+  if (!req.session.marcas) {
+    req.session.marcas = [
+      { nombre: "CaféOrigenCauca", marca: "Juan Valdez", img: "https://comprocafedecolombia.com/wp-content/uploads/2021/05/34.-CAUCA-454-G-FRONT.jpg" },
+      { nombre: "SierraNevada", marca: "Juan Valdez", img: "https://comprocafedecolombia.com/wp-content/uploads/2021/05/34.-CAUCA-454-G-FRONT.jpg" }
+    ];
+  }
+  next();
+});
 
 // Ruta para obtener la lista de marcas de café
 app.get('/marcas', (req, res) => {
-  const marcas = JSON.parse(fs.readFileSync('marcas.json', 'utf-8'));
-  res.json(marcas);
+  res.json(req.session.marcas);
 });
 
 // Ruta para agregar una nueva marca de café
 app.post('/marcas', (req, res) => {
   const nuevaMarca = req.body;
-  const marcas = JSON.parse(fs.readFileSync('marcas.json', 'utf-8'));
-  marcas.push(nuevaMarca);
-  fs.writeFileSync('marcas.json', JSON.stringify(marcas, null, 2));
+  req.session.marcas.push(nuevaMarca);
   res.json({ message: 'Marca de café agregada correctamente' });
 });
 
@@ -35,12 +41,10 @@ app.post('/marcas', (req, res) => {
 app.put('/marcas/:nombre', (req, res) => {
   const nombre = req.params.nombre;
   const nuevaInfo = req.body;
-  const marcas = JSON.parse(fs.readFileSync('marcas.json', 'utf-8'));
-  const index = marcas.findIndex(marca => marca.nombre === nombre);
+  const index = req.session.marcas.findIndex(marca => marca.nombre === nombre);
 
   if (index !== -1) {
-    marcas[index] = { ...marcas[index], ...nuevaInfo };
-    fs.writeFileSync('marcas.json', JSON.stringify(marcas, null, 2));
+    req.session.marcas[index] = { ...req.session.marcas[index], ...nuevaInfo };
     res.json({ message: 'Marca de café editada correctamente' });
   } else {
     res.status(404).json({ message: 'Marca de café no encontrada' });
@@ -50,20 +54,16 @@ app.put('/marcas/:nombre', (req, res) => {
 // Ruta para eliminar una marca de café por su nombre
 app.delete('/marcas/:nombre', (req, res) => {
   const nombre = req.params.nombre;
-  const marcas = JSON.parse(fs.readFileSync('marcas.json', 'utf-8'));
-  const index = marcas.findIndex(marca => marca.nombre === nombre);
+  const index = req.session.marcas.findIndex(marca => marca.nombre === nombre);
 
   if (index !== -1) {
-    marcas.splice(index, 1);
-    fs.writeFileSync('marcas.json', JSON.stringify(marcas, null, 2));
+    req.session.marcas.splice(index, 1);
     res.json({ message: 'Marca de café eliminada correctamente' });
   } else {
     res.status(404).json({ message: 'Marca de café no encontrada' });
   }
 });
 
-app.listen(process.env.PORT || 2800, () => {
+app.listen(PORT, () => {
   console.log(`Servidor en ejecución en http://localhost:${PORT}`);
 });
-
-module.exports = app
